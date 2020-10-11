53c2962ba6f0952925dd38a32d07d7bc6803aeae
Dawid <dawidsobierajski@gmail.com>
2020-10-11 19:20:07 +0100

    prints all rates with a specified currency as base

--- Currencyconverter.py
+++ Currencyconverter.py
@@ -2,11 +2,26 @@ import json
 import requests
 import urllib.parse
 
-url = 'https://api.exchangeratesapi.io/latest'
+url_base = 'https://api.exchangeratesapi.io/{}'
 
-json_data = requests.get(url).json()
+def rates_with_base():
+    
+    base = input("What base currency would you like to use? ")
+    formatted_url = url_base.format('latest?')
+    url = formatted_url + urllib.parse.urlencode({"base": base})
+    Json_Rates = requests.get(url).json()
 
-print("For 1 " + json_data['base'] + " You can have")
-print()
-print( str(json_data['rates']['PLN']) + " Polish Zloty")
+    print ("With " + base + " as the base todays rates are")
+    print ("----------------------------------------------")
+    print ()
+    
+    for each in Json_Rates['rates']:
+        print (each + ": " + str(Json_Rates['rates'][each]))
 
+print (" Welcome to Currency info ")
+print ("--------------------------")
+
+usr_question = input ("What currency questions do you have? ")
+
+if usr_question == "rates" or usr_question == "Rates":
+    rates_with_base()
