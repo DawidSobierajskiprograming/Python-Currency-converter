b09cbbdae71bdfead283080391449167758d1f35
Dawid <dawidsobierajski@gmail.com>
2020-10-12 13:57:05 +0100

    can input date in the dd/mm/yyyy format and will format to a dd-mm-yyyy format.

--- Currencyconverter.py
+++ Currencyconverter.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import urllib.parse
+import datetime
+from datetime import *
 
 url_base = 'https://api.exchangeratesapi.io/{}'
 
@@ -18,10 +20,23 @@ def rates_with_base():
     for each in Json_Rates['rates']:
         print (each + ": " + str(Json_Rates['rates'][each]))
 
+def rates_at_date():
+
+    request_date = input("What Date would like to have information on? ")
+    date_formatted = datetime.strptime(request_date, '%d/%m/%Y')
+    print (date_formatted)
+
 print (" Welcome to Currency info ")
 print ("--------------------------")
 
-usr_question = input ("What currency questions do you have? ")
+while True:
+    usr_question = input ("What currency questions do you have? ")
 
-if usr_question == "rates" or usr_question == "Rates":
-    rates_with_base()
+    if usr_question == "rates" or usr_question == "Rates":
+        rates_with_base()
+    
+    elif usr_question == "date":
+        rates_at_date()
+    
+    elif usr_question == "q" or usr_question == "quit":
+        break
